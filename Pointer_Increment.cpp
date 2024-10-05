290dab5863218fd671ed769b75af5bc39133c22b
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-05 20:20:04 +0530

    Add files via upload

---
+++ Pointer_Increment.cpp
@@ -0,0 +1,15 @@
+//Sakshi Sonawane
+//PRN:24070123505
+#include<iostream>
+using namespace std;
+int main(){
+    int *aptr;
+    int a=10;
+   /* int arr[5]={10,20,30,40,50};
+    for(int i=0;i<5;i++){
+        arr[i]=*aptr;*/
+    cout<<
+    aptr<<endl;
+    aptr++;
+    cout<<aptr<<endl;
+ }
