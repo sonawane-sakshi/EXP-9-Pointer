990b0d24e571f3bba2d51f2b143fdb27093a0f19
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-05 15:44:20 +0530

    Add files via upload

---
+++ Pointer_Basics.cpp
@@ -0,0 +1,12 @@
+//Sakshi Sonawane 
+//PRN:24070123505
+#include<iostream>
+using namespace std;
+int main(){
+   int a=10;
+   int*aptr;
+   aptr=&a;
+   cout<<&a<<endl;
+   cout<<aptr<<endl;
+   cout<<*aptr<<endl; 
+}
